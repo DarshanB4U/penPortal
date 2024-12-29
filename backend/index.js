30b030e4a5b47f279573cafdbc86f7f3fbc637c4
DarshanB4U <darshanbondre0@gmail.com>
2024-12-30 00:48:05 +0530

    removed hard coded article values

--- backend/index.js
+++ backend/index.js
@@ -9,52 +9,10 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-let articles = [
-  {
-    authorName: "Alice Johnson",
-    publishDate: "2024-11-01",
-    title: "Introduction to Node.js",
-    content: "Node.js is a JavaScript runtime built on Chrome's V8 engine.",
-  },
-  {
-    authorName: "Bob Smith",
-    publishDate: "2024-11-15",
-    title: "Understanding REST APIs",
-    content: "REST APIs use HTTP requests to access and manipulate data.",
-  },
-  {
-    authorName: "Charlie Brown",
-    publishDate: "2024-12-01",
-    title: "Future of AI",
-    content:
-      "AI is shaping the future of technology with machine learning and deep learning.",
-  },
-  {
-    authorName: "Diana Prince",
-    publishDate: "2024-12-05",
-    title: "A Guide to JSON",
-    content: "JSON is a lightweight format for storing and transporting data.",
-  },
-  {
-    authorName: "Eve Torres",
-    publishDate: "2024-12-10",
-    title: "JavaScript Basics",
-    content:
-      "JavaScript is a versatile language used for both client-side and server-side development.",
-  },
-  {
-    authorName: "rahul raj",
-    publishDate: "2024-12-05",
-    title: "exam and code ",
-    content:
-      " hi my name is rahul raj an i am from school  of tumsar named shrinbai neterwala school JSON is a lightweight format for storing and transporting data.",
-  },
-];
-
 app.get("/api/articles", authMiddlware, async (req, res) => {
   const data = await getfeed();
-  
-  articles = data
+
+  const articles = data;
   res.json({ articles });
 });
 
