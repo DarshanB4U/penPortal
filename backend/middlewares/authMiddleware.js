72543dd716a5e04c33c771b92659f24a7b50a6ee
DarshanB4U <darshanbondre0@gmail.com>
2025-03-30 17:20:48 +0530

    deleted duplicated file

--- backend/middlewares/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
-
-const authMiddleware = function (req, res, next) {
-  // ... existing code ...
-};
-
-module.exports = { authMiddleware } 
